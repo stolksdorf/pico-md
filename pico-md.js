680b7ef6d0d4a2b682c6066b7f4aa535a0c63320
scott-tolksdorf <scott.tolksdorf@applyboard.com>
2022-02-18 23:45:01 -0500

    I think it is finally working

--- pico-md.js
+++ pico-md.js
@@ -110,12 +110,12 @@ const Rules = Object.values({
 	// 		}).join('\n')}\n</table>`;
 	// 	}
 	// ],
-	List : [/(^[ \t]*([0-9]+\.|-)(.*)\n?)+/m,
+	List : [/(^[ \t]*([0-9]+\.|-)(.*)\n?){2,}/m,
 		(text)=>{
 
 			console.log(text)
 
-
+			/*
 			const lines = text.trim().split('\n').map(line=>{
 				let [_, indent, type, text] = /(\s*)(-|[0-9]+\.) ?(.*)/.exec(line);
 				return {
@@ -177,10 +177,151 @@ const Rules = Object.values({
 				}
 			})
 
+			*/
+
+			/*
+
+			const lines = text.trim().split('\n').map(line=>{
+				let [_, indent, type, text] = /(\s*)(-|[0-9]+\.) ?(.*)/.exec(line);
+				return {
+					type: type=='-'?'ul':'ol',
+					idt:indent.length,
+					text:inline(text)
+				}
+			});
+
+			//lines.push({idt:-1});
+
+			let depth =0;
+			let foo = [];
+			let bar = []
+			let stack = [];
+			lines.map(({type, idt, text}, idx)=>{
+				if(!stack[0] || idt > stack[0].idt){
+					foo.push(`${i(depth)}<${type}>`);
+										bar.push(`<${type}>`);
+					depth += 2;
+					stack.unshift({type, idt});
+				}else if(idt == stack[0].idt){
+					foo.push(`${i(depth-2)}</li>`);
+					bar.push(`</li>`);
+					depth -= 2;
+				}else if(idt < stack[0].idt){
+					while(idt < stack[0].idt){
+						foo.push(`${i(depth-2)}</li>`);
+						bar.push(`</li>`);
+						foo.push(`${i(depth-2)}</${stack[0].type}>`);
+						bar.push(`</${stack[0].type}>`);
+						foo.push(`${i(depth-4)}</li>`);
+						bar.push(`</li>`);
+						depth -= 4;
+						stack.shift();
+					}
+				}
+
+
+				//if(!lines[idx+1] || lines[idx+1].idt > idt){
+					foo.push(`${i(depth)}<li>`);
+					bar.push(`<li>`);
+					foo.push(`${i(depth+2)}|${text}`);
+					bar.push(`|${text}`);
+					depth +=2
+				//}else{
+				//	foo.push(`${i(depth)}<li>${text}</li>`)
+
+			});
+			while(stack.length){
+				//if(stack.length)
+				foo.push(`${i(depth-2)}</li>`);
+					bar.push(`</li>`);
+				foo.push(`${i(depth-4)}</${stack[0].type}>`);
+				bar.push(`</${stack[0].type}>`);
+
+				depth -=2;
+
+				stack.shift();
+			}
+
+			*/
 
 
 
-			console.log('-----')
+			let stack = []
+			// const lines = text.trim().split('\n').map(line=>{
+			// 	let [_, idt, type, text] = /(\s*)(-|[0-9]+\.) ?(.*)/.exec(line);
+			// 	type = type=='-'?'ul':'ol';
+			// 	idt = idt.length;
+
+			// 	if(!stack[0] || idt > stack[0].idt){
+			// 		res.push(`<${type}>`);
+			// 		// res.push(`<li>`);
+			// 		// res.push(text);
+			// 		stack.unshift({type, idt});
+			// 	}else if(idt == stack[0].idt){
+			// 		res.push(`</li>`);
+			// 		// res.push(`<li>`);
+			// 		// res.push(text);
+			// 	}else{
+			// 		while(idt < stack[0].idt){
+			// 			res.push(`</li>`);
+			// 			res.push(`</${stack[0].type}>`);
+			// 			res.push(`</li>`);
+			// 			stack.shift();
+			// 		}
+			// 		// res.push(`<li>`);
+			// 		// res.push(text);
+			// 	}
+			// 	res.push(`<li>`);
+			// 	res.push(text);
+			// });
+
+
+			let tokens = text.trim().split('\n').reduce((acc,line)=>{
+				let [_, idt, type, text] = /(\s*)(-|[0-9]+\.) ?(.*)/.exec(line);
+				type = type=='-'?'ul':'ol';
+				idt = idt.length;
+
+				if(!stack[0] || idt > stack[0].idt){
+					acc.pop();
+					acc.push(`<${type}>`);
+					stack.unshift({type, idt});
+				}else if(idt < stack[0].idt){
+					while(idt < stack[0].idt){
+						acc = acc.concat([`</${stack[0].type}>`, '</li>']);
+						stack.shift();
+					}
+				}
+				return acc.concat([`<li>`, text, '</li>']);
+			}, []);
+
+			//tokens = tokens.concat(stack.map(({type})=>`</${stack[0].type}>`).join('</li>'));
+
+			stack.map(({type})=>{
+				tokens = tokens.concat([`</${stack[0].type}>`, `</li>`])
+			});
+			tokens.pop();
+
+
+			// while(stack.length){
+			// 	res = res.concat([`</${stack[0].type}>`, '</li>']);
+			// 	// res.push(`</li>`);
+			// 	// res.push(`</${stack[0].type}>`);
+			// 	stack.shift();
+			// }
+
+
+			console.log('---')
+
+			//res.map(x=>console.log(x))
+
+			let temp = 0;
+			tokens.map(x=>{
+				if(/<\/(ul|li|ol)>/.test(x)) temp -=2;
+				console.log(`${' '.repeat(temp)}${x}`);
+				if(/<(ul|li|ol)>/.test(x)) temp +=2;
+			})
+
+			console.log('#########')
 
 		}
 	],
@@ -316,11 +457,14 @@ okay cool! }}
 */
 let res = md(`
 
+- one
+- two
+- three
 
 - one
   - two
     - three!
-
+- four
 
 - nested
   1. with differnt
@@ -330,6 +474,39 @@ let res = md(`
 
 
 `, {allowHTML: true, meta :false});
+
+
+// ['ul', [
+// 	'one',
+// 	'two',
+// 	'three'
+// ]]
+
+// ['ul', [
+// 	'one',
+// 	['ul', [
+// 		'two',
+// 		['ul', [
+// 			'three'
+// 		]]
+// 	]]
+// 	'four'
+// ]]
+
+// /////////
+
+// ['ul', [
+// 	['one'],
+// 	['two'],
+// 	['three']
+// ]]
+
+// ['ul', [
+// 	['one', 'ul',
+// 		['two'],
+// 	],
+// 	['four']
+// ]]
 
 
 //console.log(res)
